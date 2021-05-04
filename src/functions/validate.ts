5b3beec51a264f0f3d0994886c5752a0c7f15a26
Eliseu Videira <eliseuvideira1@gmail.com>
2021-05-04 11:35:48 -0300

    :bug: fixed typing error

--- src/functions/validate.ts
+++ src/functions/validate.ts
@@ -1,15 +1,23 @@
 import { ValidationError } from "apollo-server-errors";
-import { ResolverFn } from "graphql-subscriptions";
 import { ObjectSchema } from "joi";
-import { resolver } from "./resolver";
+import { Context } from "../types/Context";
+import { resolver, ResolverFn } from "./resolver";
 
-export const validate = (resolv: ResolverFn, schema: ObjectSchema) =>
-  resolver(async (parent, args, ctx, ...rest) => {
+export const validate = <
+  Parent = any,
+  Args = any,
+  Ctx extends Context = Context,
+  Result = any
+>(
+  resolv: ResolverFn<Parent, Args, Ctx, Result>,
+  schema: ObjectSchema,
+) =>
+  resolver<Parent, Args, Ctx, Result>(async (parent, args, ctx, ...rest) => {
     let value;
     try {
       value = await schema.validateAsync(args);
     } catch (err) {
       throw new ValidationError(err.message);
     }
-    resolv(parent, value, ctx, ...rest);
+    return resolv(parent, value, ctx, ...rest);
   });
